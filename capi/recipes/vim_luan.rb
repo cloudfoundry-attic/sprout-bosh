3a88d9513e274468b53fb0d53ca99ecbc07d2ca3
Dan Wendorf and Jeff Pak <dwendorf@pivotal.io>
2015-07-13 11:33:03 -0700

    Used Luan's vim config
    [#98835342]

---
+++ capi/recipes/vim_luan.rb
@@ -0,0 +1,23 @@
+package 'vim'
+sprout_homebrew_cask 'macvim'
+
+vimfiles = ::File.join(node['sprout']['home'], node['workspace_directory'], 'vimfiles')
+git vimfiles do
+  repository 'http://github.com/luan/vimfiles.git'
+  revision 'master'
+  user node['sprout']['user']
+  enable_submodules true
+  action :checkout
+end
+
+dotvim = ::File.join(node['sprout']['home'], '.vim')
+
+link dotvim do
+  to vimfiles
+  owner node['sprout']['user']
+end
+
+execute './install --non-interactive' do
+  cwd dotvim
+  user node['sprout']['user']
+end
