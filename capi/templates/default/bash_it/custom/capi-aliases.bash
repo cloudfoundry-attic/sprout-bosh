a6675189d9560efbc0ec6832d5b3a2a12d13c553
Dan Wendorf <dwendorf@pivotal.io>
2015-07-14 16:14:58 -0700

    Add qnd-deploy and qnd-deploy-manifest aliases
    Signed-off-by: Raina Masand <rmasand@pivotal.io>

--- capi/templates/default/bash_it/custom/capi-aliases.bash
+++ capi/templates/default/bash_it/custom/capi-aliases.bash
@@ -1,3 +1,7 @@
 alias resprout='(~/workspace/sprout-capi && git pull && bundle exec soloist)'
 alias create-lite='bosh create release --force && bosh target 192.168.50.4 lite && ./bosh-lite/make_manifest && bosh -n upload release && bosh -n deploy'
 alias cats='(cd ~/workspace/cf-release/src/acceptance-tests && CONFIG=$PWD/integration_config.json bin/test -nodes 4)'
+
+# Bosh-lite setup
+alias qnd-deploy='bosh create release --force && bosh upload release && bosh -n deploy'
+alias qnd-deploy-manifest='bosh-lite/make_manifest && qnd-deploy'
