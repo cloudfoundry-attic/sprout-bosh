d5b93e2e7b1b9fa4d0df71698443b7000f3ce7d5
Dan Wendorf <dwendorf@pivotal.io>
2015-07-14 14:10:30 -0700

    Switch to git-duet instead of git-pair
    [#98835342]

--- capi/attributes/global_config.rb
+++ capi/attributes/global_config.rb
@@ -1,4 +1,4 @@
 default['sprout']['git']['global_config'] = {
     'url."git@github.com:".pushInsteadOf' => 'https://github.com/',
-    'alias.ci' => 'pair-commit',
+    'alias.ci' => 'duet-commit',
 }
