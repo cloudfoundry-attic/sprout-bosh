eeb3c5a334cdfe9fb36f395ec4119b6ea40a6c75
Dan Wendorf <dwendorf@pivotal.io>
2015-07-17 17:57:05 -0700

    Correctly install fast-bosh on Ruby 2.1.6

--- capi/recipes/fast_bosh.rb
+++ capi/recipes/fast_bosh.rb
@@ -1,3 +1,6 @@
+require 'etc'
+require 'bundler'
+
 include_recipe 'sprout-chruby'
 
 sprout_base_bash_it_custom_plugin 'bash_it/custom/fast_bosh.bash'
@@ -20,8 +23,21 @@ cookbook_file "#{fast_bosh_path}/.ruby-version" do
   mode "0644"
 end
 
-execute "install bosh cli" do
-  command "bash -l -c '$HOME/.gem/ruby/2.1.6/bin/bundle install'"
-  cwd fast_bosh_path
+execute "install bundler" do
+  command "bash -l -c 'chruby-exec 2.1.6 -- gem install bundler'"
   user node['sprout']['user']
+end
+
+ruby_block "install bosh cli" do
+  block do
+    Bundler.with_clean_env do
+      Dir.chdir(fast_bosh_path) do
+        Process.fork do
+          Process.uid = Etc.getpwnam(node['sprout']['user']).uid
+
+          `bash -l -c 'GEM_PATH= BUNDLE_GEMFILE=#{fast_bosh_path}/Gemfile chruby-exec 2.1.6 -- bundle install'`
+        end
+      end
+    end
+  end
 end
