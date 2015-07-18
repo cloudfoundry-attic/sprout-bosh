f44d187dd52a2627b73668035d946901d25fe1b8
Dan Wendorf <dwendorf@pivotal.io>
2015-07-17 17:52:08 -0700

    Install spiff from release instead of compiling from source

--- capi/recipes/spiff.rb
+++ capi/recipes/spiff.rb
@@ -1,10 +1,20 @@
-include_recipe 'sprout-osx-apps::go'
+node.default['spiff']['url'] = 'https://github.com/cloudfoundry-incubator/spiff/releases/download/v1.0.7/spiff_darwin_amd64.zip'
+node.default['spiff']['shasum'] = 'c9490a3daf362448058dd22e96c0809a1a04a9c6'
 
-execute 'install spiff' do
-  command 'go get -v -u github.com/cloudfoundry-incubator/spiff'
-  environment(
-    'GOPATH' 	 => ENV['GOPATH'] || File.join(node['sprout']['home'], 'go'),
-    'PYTHONPATH' => '/usr/local/lib/python2.7/site-packages'
-  )
-  user node['sprout']['user']
+unless File.exists?('/usr/local/bin/spiff')
+  directory Chef::Config[:file_cache_path] do
+    action :create
+    recursive true
+  end
+
+  remote_file "#{Chef::Config[:file_cache_path]}/spiff.zip" do
+    source node['spiff']['url']
+    checksum node['spiff']['shasum']
+    owner node['sprout']['user']
+  end
+
+  execute 'extract text mate to /usr/local/bin' do
+    command "unzip -o #{Chef::Config[:file_cache_path]}/spiff.zip -x __MACOSX* -d /usr/local/bin/"
+    user node['sprout']['user']
+  end
 end
