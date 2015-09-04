f5e491eb1e24cc3855a2c1b33f69e309a8a9e8b9
Dan Wendorf <dwendorf@pivotal.io>
2015-09-04 12:16:33 -0700

    Update fast_bosh to just use BOSH_USE_BUNDLER flag
    [#102181986]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- capi/recipes/fast_bosh.rb
+++ capi/recipes/fast_bosh.rb
@@ -1,43 +1,28 @@
-require 'etc'
-require 'bundler'
-
 include_recipe 'sprout-chruby'
 
 sprout_base_bash_it_custom_plugin 'bash_it/custom/fast_bosh.bash'
 
-fast_bosh_path = "#{node['sprout']['home']}/#{node['workspace_directory']}/fast-bosh"
-directory fast_bosh_path do
-  recursive true
-  owner node['sprout']['user']
+def install_bosh_cli(ruby)
+  command = Mixlib::ShellOut.new("chruby-exec #{ruby} -- gem list bosh_cli | grep bosh_cli", user: node['sprout']['user'])
+  command.run_command
+  return if command.exitstatus == 0
+
+  execute "install bundler for #{ruby}" do
+    command "bash -l -c 'chruby-exec #{ruby} -- gem install bundler'"
+    user node['sprout']['user'] unless ruby == 'system'
+  end
+
+  execute "install bosh cli for #{ruby}" do
+    command "chruby-exec #{ruby} -- gem install bosh_cli"
+    user node['sprout']['user'] unless ruby == 'system'
+  end
 end
 
-cookbook_file "#{fast_bosh_path}/Gemfile" do
-  source "fast_bosh/Gemfile"
-  user node['sprout']['user']
-  mode "0644"
-end
+rubies = node['sprout']['chruby']['rubies']
 
-cookbook_file "#{fast_bosh_path}/.ruby-version" do
-  source "fast_bosh/.ruby-version"
-  user node['sprout']['user']
-  mode "0644"
-end
-
-execute "install bundler" do
-  command "bash -l -c 'chruby-exec 2.1.6 -- gem install bundler'"
-  user node['sprout']['user']
-end
-
-ruby_block "install bosh cli" do
-  block do
-    Bundler.with_clean_env do
-      Dir.chdir(fast_bosh_path) do
-        Process.fork do
-          Process.uid = Etc.getpwnam(node['sprout']['user']).uid
-
-          `bash -l -c 'GEM_PATH= BUNDLE_GEMFILE=#{fast_bosh_path}/Gemfile chruby-exec 2.1.6 -- bundle install'`
-        end
-      end
-    end
+rubies.each do |ruby_vm, ruby_versions|
+  ruby_versions.each do |ruby_version|
+    ruby = "#{ruby_vm}-#{ruby_version}"
+    install_bosh_cli(ruby)
   end
 end
--- capi/templates/default/bash_it/custom/fast_bosh.bash
+++ capi/templates/default/bash_it/custom/fast_bosh.bash
@@ -1,4 +1,1 @@
-function bosh() {
-  GEM_PATH= BUNDLE_GEMFILE=~/workspace/fast-bosh/Gemfile chruby-exec 2.1.6 -- bundle exec bosh "$@"
-}
-export -f bosh
+export BOSH_USE_BUNDLER=true
