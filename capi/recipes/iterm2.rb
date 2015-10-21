3a95c3d011920d99f9fc624d7b2ea7c806dde900
Utako Ueda <uueda@pivotal.io>
2015-10-21 15:53:25 -0700

    Update spectacle and iterm2 custom configuration
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

---
+++ capi/recipes/iterm2.rb
@@ -0,0 +1,7 @@
+homebrew_cask 'iterm2-nightly'
+
+cookbook_file "/Users/#{node['sprout']['user']}/Library/Preferences/com.googlecode.iterm2.plist" do
+  source 'com.googlecode.iterm2.plist'
+  user node['sprout']['user']
+  mode '0600'
+end
