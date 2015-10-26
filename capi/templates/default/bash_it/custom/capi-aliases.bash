7b1333d9974b4900c6a77e54e56011dcb43ed34f
Swetha Repakula <srepaku@us.ibm.com>
2015-10-26 09:57:31 -0700

    Add v3-cats alias
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- capi/templates/default/bash_it/custom/capi-aliases.bash
+++ capi/templates/default/bash_it/custom/capi-aliases.bash
@@ -1,5 +1,6 @@
 alias resprout='(cd ~/workspace/sprout-capi && git pull && chruby-exec system -- bundle exec soloist)'
 alias cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test_default -p)'
+alias v3-cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test v3)'
 
 # Bosh-lite setup
 alias qnd-deploy='(cd ~/workspace/cf-release && bosh create release --name cf --force && bosh upload release && bosh -n deploy)'
