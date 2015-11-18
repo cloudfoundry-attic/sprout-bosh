d1747be150a681850ff320b3535cd4b8f9085acd
Swetha Repakula <srepaku@us.ibm.com>
2015-11-18 10:20:29 -0800

    Edit recreate_bosh_lite to target bosh-lite

--- capi/templates/default/bash_it/custom/recreate_bosh_lite.bash
+++ capi/templates/default/bash_it/custom/recreate_bosh_lite.bash
@@ -1,74 +1,74 @@
 function recreate_bosh_lite() {
-  (read -s -p "password: " password
-  if [[ $1 == "-u" ]]; then
-    echo -e '\nWill Update cf-release and diego-release'
-    update=true
-  fi
+  (
+    sudo true
 
-  cd ~/workspace/bosh-lite
+    update=false
+    if [[ $1 == "-u" ]]; then
+      echo -e '\nWill Update cf-release and diego-release'
+      update=true
+    fi
 
-  echo -e "\nDestroying current Bosh-Lite"
-  vagrant destroy --force
+    cd ~/workspace/bosh-lite
 
-  #pull in changes for bosh-lite
-  git pull
+    echo -e "\nDestroying current Bosh-Lite"
+    vagrant destroy --force
 
-  #update vagrant box
-  vagrant box update
+    #pull in changes for bosh-lite
+    git pull
 
-  echo "Starting up Bosh-Lite"
-  vagrant up
+    #update vagrant box
+    vagrant box update
 
-  echo "Adding route"
-  echo $password | sudo -S bin/add-route
+    echo "Starting up Bosh-Lite"
+    vagrant up
 
-  cd ~/workspace/cf-release
-  bundle install
+    echo "Adding route"
+    sudo -S bin/add-route
 
-  echo "Getting Stemcell"
-  stemcell=`bosh public stemcells | grep -o 'bosh.*-warden-boshlite-ubuntu-trusty-go_agent.tgz'`
-  bosh download public stemcell $stemcell
-  bosh upload stemcell $stemcell
-  pause 'Press [Enter] key to continue...'
+    cd ~/workspace/cf-release
 
+    bosh target lite
 
-  echo "Updating Diego-Release"
-  cd ~/workspace/diego-release
+    echo "Getting Stemcell"
+    bosh upload stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
 
-  if $update; then
-    ./scripts/update
-  fi
+    if $update; then
+      echo "Updating Diego-Release"
+      ~/workspace/diego-release/scripts/update
+    fi
 
-  cd ~/workspace/cf-release
-  echo "Deploying CF release"
+    cd ~/workspace/cf-release
+    echo "Deploying CF release"
 
-  if $update; then
-    ./scripts/update
-  fi
+    if $update; then
+      ./scripts/update
+    fi
 
-  cd ~/workspace/cf-release
-  ~/workspace/cf-release/scripts/generate-bosh-lite-dev-manifest
+    cd ~/workspace/cf-release
+    ~/workspace/cf-release/scripts/generate-bosh-lite-dev-manifest
 
-  bosh create release --name cf --force
-  bosh upload release
-  bosh -n deploy
+    bosh create release --name cf --force
+    bosh -t lite upload release
+    bosh -t lite -n deploy
 
-  echo "Uploading Garden Linux"
-  bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release
+    echo "Uploading Garden Linux"
+    bosh -t lite upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release
 
-  echo "Uploading etcd-release"
-  bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release
+    echo "Uploading etcd-release"
+    bosh -t lite upload release https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release
 
-  cd ~/workspace/diego-release
-  ./scripts/generate-bosh-lite-manifests
-  echo "Deploying Diego"
+    cd ~/workspace/diego-release
 
-  bosh deployment bosh-lite/deployments/diego.yml
-  bosh create release --force
-  bosh -n upload release
-  bosh -n deploy
+    ./scripts/generate-bosh-lite-manifests
+    echo "Deploying Diego"
 
-  cd ~/workspace/cf-release
-  rm -f $stemcell)
+    bosh -t lite deployment bosh-lite/deployments/diego.yml
+    bosh create release --force
+    bosh -t lite -n upload release
+    bosh -t lite -n deploy
+
+    cd ~/workspace/cf-release
+    rm -f $stemcell
+  )
 }
 export -f recreate_bosh_lite
