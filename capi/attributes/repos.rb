fa70b05b1d36ae53d4ab8c357ebe07df415eba81
Dan Wendorf <dwendorf@pivotal.io>
2016-02-03 11:07:11 -0800

    Remove unused attributes

--- capi/attributes/repos.rb
+++
@@ -1,9 +0,0 @@
-default['cf-runtime']['repos'] = [
-  { 'name' => 'bosh-lite' },
-  { 'name' => 'cf-release', 'branch' => 'develop' },
-  { 'name' => 'ci' },
-  { 'name' => 'deployments-runtime' },
-  { 'name' => 'runtime-checkman' },
-  { 'name' => 'runtime-credentials', 'url' => 'git@github.com:pivotal-cf/runtime-credentials' },
-  { 'name' => 'warden-test-infrastructure' },
-]
