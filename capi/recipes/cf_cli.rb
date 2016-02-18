f3ef84b981f3fd3b32bf10fbf2f5d36b75293880
Frank Kotsianas <fkotsianas@pivotal.io>
2016-02-17 17:14:59 -0800

    install cf cli v3 plugin
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

--- capi/recipes/cf_cli.rb
+++ capi/recipes/cf_cli.rb
@@ -10,3 +10,8 @@ execute 'install cf cli CLI-Recorder plugin' do
   command "cf uninstall-plugin CLI-Recorder; cf install-plugin CLI-Recorder -r CF-Community -f"
   user node['sprout']['user']
 end
+
+execute 'install cf cli v3-cli-plugin plugin' do
+  command "cf uninstall-plugin v3_beta; cf install-plugin https://github.com/cloudfoundry/v3-cli-plugin/blob/master/bin/OSX_binary?raw=true -f"
+  user node['sprout']['user']
+end
