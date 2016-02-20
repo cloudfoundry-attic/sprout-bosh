c26f449256c3f83cc2db960a14bb31ea69eaa1be
Utako Ueda <uueda@pivotal.io>
2016-02-19 17:17:37 -0800

    qnd-deploy[-manifest] now updates/deploys diego
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- capi/templates/default/bash_it/custom/capi-aliases.bash
+++ capi/templates/default/bash_it/custom/capi-aliases.bash
@@ -3,8 +3,8 @@ alias cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance
 alias v3-cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test v3)'
 
 # Bosh-lite setup
-alias qnd-deploy='(cd ~/workspace/cf-release && bosh --parallel 10 sync blobs && bosh create release --name cf --force && bosh upload release && bosh -n deploy)'
-alias qnd-deploy-manifest='(cd ~/workspace/cf-release && scripts/generate-bosh-lite-dev-manifest && qnd-deploy)'
+alias qnd-deploy='(cd ~/workspace/cf-release && bosh --parallel 10 sync blobs && bosh create release --name cf --force && bosh upload release && bosh -n deploy && cd ~/workspace/diego-release && scripts/update && scripts/deploy)'
+alias qnd-deploy-manifest='(cd ~/workspace/cf-release && scripts/generate-bosh-lite-dev-manifest && qnd-deploy && cd ~/workspace/diego-release && scripts/update && scripts/deploy)'
 
 #FASD
 alias v='fasd -e vim'
