601b5f5ff79183ea1821eeb95a788dc4b3675d6f
Dan Wendorf <dwendorf@pivotal.io>
2016-02-26 14:01:55 -0800

    Remove diego work from qnd scripts, add qnd-deploy-diego
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- capi/templates/default/bash_it/custom/capi-aliases.bash
+++ capi/templates/default/bash_it/custom/capi-aliases.bash
@@ -3,8 +3,9 @@ alias cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance
 alias v3-cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test v3)'
 
 # Bosh-lite setup
-alias qnd-deploy='(cd ~/workspace/cf-release && bosh --parallel 10 sync blobs && bosh create release --name cf --force && bosh upload release && bosh -n deploy && cd ~/workspace/diego-release && scripts/update && scripts/deploy)'
-alias qnd-deploy-manifest='(cd ~/workspace/cf-release && scripts/generate-bosh-lite-dev-manifest && qnd-deploy && cd ~/workspace/diego-release && scripts/update && scripts/deploy)'
+alias qnd-deploy='(cd ~/workspace/cf-release && bosh --parallel 10 sync blobs && bosh create release --name cf --force && bosh upload release && bosh -n deploy'
+alias qnd-deploy-diego='(cd ~/workspace/diego-release && bosh --parallel 10 sync blobs && scripts/update && scripts/deploy && bosh deployment ~/workspace/cf-release/bosh-lite/deployments/cf.yml)'
+alias qnd-deploy-manifest='(cd ~/workspace/cf-release && scripts/generate-bosh-lite-dev-manifest && qnd-deploy)'
 
 #FASD
 alias v='fasd -e vim'
