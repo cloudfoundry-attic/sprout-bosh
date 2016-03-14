b087808f62cd83c9379886fedd3369d824cd992d
Dan Wendorf <github@danwendorf.com>
2016-03-14 11:07:59 -0700

    Get fly from capi instead of runtime

--- capi/recipes/concourse_cli.rb
+++ capi/recipes/concourse_cli.rb
@@ -1,5 +1,5 @@
 remote_file '/usr/local/bin/fly' do
-  source 'https://runtime.ci.cf-app.com/api/v1/cli?arch=amd64&platform=darwin'
+  source 'https://capi.ci.cf-app.com/api/v1/cli?arch=amd64&platform=darwin'
   owner node['sprout']['user']
   mode '0755'
 end
