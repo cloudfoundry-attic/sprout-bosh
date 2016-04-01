0be252f2129c98a023bbd5d7d11f976d134d77c2
Amelia Downs <adowns@pivotal.io>
2016-04-01 10:16:56 -0700

    run v3-cats in parallel
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- capi/templates/default/bash_it/custom/capi-aliases.bash
+++ capi/templates/default/bash_it/custom/capi-aliases.bash
@@ -1,6 +1,6 @@
 alias resprout='(cd ~/workspace/sprout-capi && git pull && chruby-exec system -- bundle exec soloist)'
 alias cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test_default -p)'
-alias v3-cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test v3)'
+alias v3-cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test -p v3)'
 
 # Bosh-lite setup
 alias qnd-deploy='(cd ~/workspace/cf-release && bosh --parallel 10 sync blobs && bosh create release --name cf --force && bosh upload release && bosh -n deploy)'
