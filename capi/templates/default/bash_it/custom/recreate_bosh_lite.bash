715b84da1e5e74a4a1ac3231d47087db37caef7d
Dan Wendorf <dwendorf@pivotal.io>
2016-04-04 14:38:20 -0700

    Correctly use aria2c to download files to absolute directory
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- capi/templates/default/bash_it/custom/recreate_bosh_lite.bash
+++ capi/templates/default/bash_it/custom/recreate_bosh_lite.bash
@@ -1,5 +1,6 @@
 function recreate_bosh_lite() {
   (
+    cache_directory="$HOME/Downloads"
     set -e
     sudo true
 
@@ -33,17 +34,17 @@ function recreate_bosh_lite() {
     stemcell_data=`curl --retry 5 -s -L https://bosh.io/api/v1/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent`
     stemcell_version=`jq '.[0].version' --raw-output <<< $stemcell_data`
     stemcell_url=`jq '.[0].regular.url' --raw-output <<< $stemcell_data`
-    stemcell_filename="$HOME/Downloads/bosh-stemcell-$stemcell_version-warden-boshlite-ubuntu-trusty-go_agent.tgz"
+    stemcell_filename="bosh-stemcell-$stemcell_version-warden-boshlite-ubuntu-trusty-go_agent.tgz"
 
-    if [ ! -f $stemcell_filename ]; then
+    if [ ! -f "$cache_directory/$stemcell_filename" ]; then
       echo "Downloading stemcell version $stemcell_version"
-      aria2c -x 16 -s 16 -o $stemcell_filename $stemcell_url
+      aria2c -x 16 -s 16 -d $cache_directory -o $stemcell_filename $stemcell_url
     else
       echo "Stemcell version $stemcell_version already exists"
     fi
 
     echo "Uploading Stemcell"
-    bosh -t lite upload stemcell $stemcell_filename
+    bosh -t lite upload stemcell "$cache_directory/$stemcell_filename"
 
     if $update; then
       echo "Updating Diego-Release"
@@ -68,32 +69,32 @@ function recreate_bosh_lite() {
     garden_release_data=`curl --retry 5 -s -L https://bosh.io/api/v1/releases/github.com/cloudfoundry-incubator/garden-linux-release`
     garden_release_version=`jq '.[0].version' --raw-output <<< $garden_release_data`
     garden_release_url=`jq '.[0].url' --raw-output <<< $garden_release_data`
-    garden_release_filename="$HOME/Downloads/garden-linux-release-$garden_release_version.tgz"
+    garden_release_filename="garden-linux-release-$garden_release_version.tgz"
 
-    if [ ! -f $garden_release_filename ]; then
+    if [ ! -f "$cache_directory/$garden_release_filename" ]; then
       echo "Downloading garden release version $garden_release_version"
-      aria2c -x 16 -s 16 -o $garden_release_filename $garden_release_url
+      aria2c -x 16 -s 16 -d $cache_directory -o $garden_release_filename $garden_release_url
     else
       echo "Garden release version $garden_release_version already exists"
     fi
 
     echo "Uploading Garden Linux"
-    bosh -t lite upload release $garden_release_filename
+    bosh -t lite upload release "$cache_directory/$garden_release_filename"
 
     etcd_release_data=`curl --retry 5 -s -L https://bosh.io/api/v1/releases/github.com/cloudfoundry-incubator/etcd-release`
     etcd_release_version=`jq '.[0].version' --raw-output <<< $etcd_release_data`
     etcd_release_url=`jq '.[0].url' --raw-output <<< $etcd_release_data`
-    etcd_release_filename="$HOME/Downloads/etcd-release-$etcd_release_version.tgz"
+    etcd_release_filename="etcd-release-$etcd_release_version.tgz"
 
-    if [ ! -f $etcd_release_filename ]; then
+    if [ ! -f "$cache_directory/$etcd_release_filename" ]; then
       echo "Downloading etcd release version $etcd_release_version"
-      aria2c -x 16 -s 16 -o $etcd_release_filename $etcd_release_url
+      aria2c -x 16 -s 16 -d $cache_directory -o $etcd_release_filename $etcd_release_url
     else
       echo "Etcd release version $etcd_release_version already exists"
     fi
 
     echo "Uploading etcd-release"
-    bosh -t lite upload release $etcd_release_filename
+    bosh -t lite upload release "$cache_directory/$etcd_release_filename"
 
     cd ~/workspace/diego-release
 
