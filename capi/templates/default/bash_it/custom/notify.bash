cf970d653d1713fb55c662f70d8213393f6e2180
Urvashi Reddy <ureddy@pivotal.io>
2016-06-04 10:40:44 -0700

    Notify opens different images based commands

--- capi/templates/default/bash_it/custom/notify.bash
+++ capi/templates/default/bash_it/custom/notify.bash
@@ -3,20 +3,26 @@ function notify() {
     echo "notify requires an argument. For example:"
     echo -en "\n"
     echo "notify v3-cats"
+    echo "notify 'sleep 1'"
     echo "notify 'cd cf-release && scripts/deploy'"
     return
   fi
 
   eval "$1"
   local CMD_EXIT_CODE=$(echo $?)
+  local CAT_OR_NOT="default"
   get_last_names
 
+  if [[ "$1" =~ "cat" ]]; then
+    CAT_OR_NOT="cat"
+  fi
+
   if [ $CMD_EXIT_CODE -eq 130 ]; then
-    exit 0
+    return 0
   elif [ $CMD_EXIT_CODE -eq 0 ]; then
-    open "http://get-back-to-work-meow.cfapps.io/cat-success/${last_names}"
+    open "http://get-back-to-work-meow.cfapps.io/${CAT_OR_NOT}-success/${LAST_NAMES}"
   else
-    open "http://get-back-to-work-meow.cfapps.io/cat-fail/${last_names}"
+    open "http://get-back-to-work-meow.cfapps.io/${CAT_OR_NOT}-fail/${LAST_NAMES}"
   fi
 }
 
@@ -26,10 +32,10 @@ function get_last_names(){
   local author1array=($author1)
 
   if [[ -z $author2 ]]; then
-    last_names="${author1array[1]}"
+    LAST_NAMES="${author1array[1]}"
   else
     local author2array=($author2)
-    last_names="${author1array[1]}%20and%20${author2array[1]}"
+    LAST_NAMES="${author1array[1]}%20and%20${author2array[1]}"
   fi
 }
 
